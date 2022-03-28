1333737ec47f3e51cbccf454c1bbe347cb60b518
mikhail <mikhailgrigoriev92@gmail.com>
2022-03-28 15:03:49 -0400

    Get Flight Index search results to display
    using flight_params

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,16 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.all
     @airport_options = Airport.all.map{ |airport| [ airport.name, airport.id ] }
+    if flight_params.empty?
+      @flight_results = []
+    else
+      @flight_results = Flight.all.where(flight_params)
+    end
+  end
+
+  private 
+
+  def flight_params
+    params.permit(:departing_airport_id, :arriving_airport_id)
   end
 end
