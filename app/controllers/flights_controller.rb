568eb0b0de4a93cca4b01b14cf6e2797ee8d1201
mikhail <mikhailgrigoriev92@gmail.com>
2022-03-28 11:59:05 -0400

    Fix Flight Index select options
    By properly using @airport_options instead of @flight_options inside of the FlightsController index action

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,6 @@
 class FlightsController < ApplicationController
   def index
     @flights = Flight.all
-    @flight_options = @flights.map{ |flight| [ flight.arriving_airport_id, flight.departing_airport_id ] }
+    @airport_options = Airport.all.map{ |airport| [ airport.name, airport.id ] }
   end
 end
