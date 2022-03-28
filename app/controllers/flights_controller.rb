88c3523267eab7a88aed6cf3a852dc15540cd4ea
mikhail <mikhailgrigoriev92@gmail.com>
2022-03-28 11:53:06 -0400

    Create initial Flights Index View
    Currently not functional, @flight_options in FlightsController does not properly receive the id's of the arriving and departing airports

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,6 @@
 class FlightsController < ApplicationController
   def index
     @flights = Flight.all
+    @flight_options = @flights.map{ |flight| [ flight.arriving_airport_id, flight.departing_airport_id ] }
   end
 end
