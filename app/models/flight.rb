3d837c6a98b8fe7968f31728dc3078a86fb26e90
mikhail <mikhailgrigoriev92@gmail.com>
2022-03-29 08:56:04 -0400

    Change Flight Index View search dates to be unique
    Not currently searching properly due to the change to month/day

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,4 +2,7 @@ class Flight < ApplicationRecord
   belongs_to :arriving_airport, class_name: "Airport", foreign_key: :arriving_airport_id
   belongs_to :departing_airport, class_name: "Airport", foreign_key: :departing_airport_id
 
+  def start_date_formatted
+    self.start.strftime("%m/%d/%Y")
+  end
 end
