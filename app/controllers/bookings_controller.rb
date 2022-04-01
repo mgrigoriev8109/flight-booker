21534f28be7354c42969ede14fa0f45f7587d962
mikhail <mikhailgrigoriev92@gmail.com>
2022-04-01 11:41:40 -0400

    Create Booking routes and begin new action brainstorm

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,11 @@
 class BookingsController < ApplicationController
+  def new
+    #will receive the flight ID and passenger number parameter
+    #will use this to help render a form for a new booking
+    #this form should display chosen date, airports, flight id
+    #along with a set of fields to enter personal information
+
+    #I can create a new blank Passenger object in this controller for each passenger
+    #then use fields_for in the view to set up the sub-forms
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :flights
   resources :airports
+  resources :bookings
   # Defines the root path route ("/")
   root "flights#index"
      
