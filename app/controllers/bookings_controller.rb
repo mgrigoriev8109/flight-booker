3bbb6710da67bf5c718e4dcf12d5684a3a1e7f89
mikhail <mikhailgrigoriev92@gmail.com>
2022-04-02 12:10:06 -0400

    Work on Booking controller
    Parameters seem to be passed into controller, next step will be properly using them to create a new booking.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,6 @@
 class BookingsController < ApplicationController
   def new
+    flight_id = booking_params
     debugger
     #will receive the flight ID and passenger number parameter
     #will use this to help render a form for a new booking
@@ -9,4 +10,16 @@ class BookingsController < ApplicationController
     #I can create a new blank Passenger object in this controller for each passenger
     #then use fields_for in the view to set up the sub-forms
   end
+
+  private 
+
+  def booking_params
+    params.permit(:booking)
+    
+  end
+
+  def passenger_params
+    params.permit(:passengers)
+  end
+
 end
