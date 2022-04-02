0cd20e5155e172467be4efed261f49ad48f10c11
mikhail <mikhailgrigoriev92@gmail.com>
2022-04-02 10:55:24 -0400

    Update migrations for Bookings and Passenger

--- db/migrate/20220401151511_create_bookings.rb
+++ db/migrate/20220401151511_create_bookings.rb
@@ -1,7 +1,9 @@
 class CreateBookings < ActiveRecord::Migration[7.0]
   def change
     create_table :bookings do |t|
-
+      t.belongs_to :flight
+      t.belongs_to :passenger
+      
       t.timestamps
     end
   end
--- db/migrate/20220401155236_create_passengers.rb
+++ db/migrate/20220401155236_create_passengers.rb
@@ -1,6 +1,8 @@
 class CreatePassengers < ActiveRecord::Migration[7.0]
   def change
     create_table :passengers do |t|
+      t.string :name
+      t.string :email
 
       t.timestamps
     end
