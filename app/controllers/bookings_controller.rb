2336e5894520e605caff99b3c250b4b72b16befd
mikhail <mikhailgrigoriev92@gmail.com>
2022-04-05 13:10:38 -0400

    Write BookingController #create and #show actions
    Also modifying the booking_params to require the :booking and permit the passenger_attributes

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,20 +1,34 @@
 class BookingsController < ApplicationController
   def new
-    @flight = Flight.find(booking_params[:flight_id])
+    @flight = Flight.find(params[:flight_id])
     @booking = Booking.new
-    @number_of_passengers = passenger_params[:quantity].to_i
+    @number_of_passengers = quantity_params[:quantity].to_i
     @number_of_passengers.times do
       @booking.passengers.build
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+  
+  def create
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+      redirect_to @booking
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   private 
 
   def booking_params
-    params.permit(:flight_id)
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:id, :name, :email])
   end
 
-  def passenger_params
+  def quantity_params
     params.permit(:quantity)
   end
 
