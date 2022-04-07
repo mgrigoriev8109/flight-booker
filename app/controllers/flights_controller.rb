ee3e896e40903c4c2233a45bc944b665151a5eee
mikhail <mikhailgrigoriev92@gmail.com>
2022-04-07 14:03:14 -0400

    Begin styling booking form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,19 +1,21 @@
 class FlightsController < ApplicationController
   def index
     @flights = Flight.all
-    @airport_options = Airport.all.map{ |airport| [ airport.name, airport.id ] }
 
     if flight_params.empty?
       @flight_results = []
-    else
-      departure_date = departure_time_param[:start].to_date
-      @flight_results = Flight.all.where(flight_params).where(start: departure_date.midnight..(departure_date.midnight + 1.day))
+    else      
+      @flight_results = find_flights
     end
 
-    @number_of_passengers = passenger_params
   end
 
   private 
+
+  def find_flights
+    departure_date = departure_time_params[:start].to_date
+    Flight.all.where(flight_params).where(start: departure_date.midnight..(departure_date.midnight + 1.day))
+  end
 
   def flight_params
     params.permit(:departing_airport_id, :arriving_airport_id)
@@ -23,7 +25,7 @@ class FlightsController < ApplicationController
     params.permit(:number_of_passengers)
   end
 
-  def departure_time_param
+  def departure_time_params
     params.permit(:start)
   end
 end
