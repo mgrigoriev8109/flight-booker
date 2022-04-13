f9212a989095d7f528fa997205fa21722d2cf9cd
mikhail <mikhailgrigoriev92@gmail.com>
2022-04-13 15:35:37 -0400

    Continue working on integration testing

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,5 @@ class Booking < ApplicationRecord
   has_many :passengers
   belongs_to :flight
   
-  accepts_nested_attributes_for :passengers
+  accepts_nested_attributes_for :passengers, reject_if: proc { |attributes| attributes['name'].blank? }
 end
